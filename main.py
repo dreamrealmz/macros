87bd366bc0779bc29e6fdb28a8f75f656badce78
sasha <ad.azarov17@gmail.com>
2023-04-05 17:32:23 +0600

    fix script creating process still need to fix mapping of buttons and share more languages

--- main.py
+++ main.py
@@ -92,9 +92,9 @@ class MainWindow(QMainWindow):
             ["esc", "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "f11", "f12"],
             ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "backspace"],
             ["Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\\"],
-            ["Caps Lock", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "Enter"],
-            ["L_shift", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "R_shift"],
-            ["L_ctrl", "L_win", "L_alt", "Space", "R_alt", "R_win", "R_ctrl"]
+            ["Caps Lock", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "enter"],
+            ["shift", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "shift_r"],
+            ["ctrl_l", "cmd", "alt_l", "space", "alt_gr", "cmd_r", "ctrl_r"]
         ]
 
         for row in range(len(buttons)):
@@ -163,114 +163,20 @@ class MainWindow(QMainWindow):
 class KeyboardListener:
     def __init__(self, keyboard_mapping=None):
         self.button_comparison = {
-            'esc': 'Key.esc',
-            'f1': 'Key.f1',
-            'f2': 'Key.f2',
-            'f3': 'Key.f3',
-            'f4': 'Key.f4',
-            'f5': 'Key.f5',
-            'f6': 'Key.f6',
-            'f7': 'Key.f7',
-            'f8': 'Key.f8',
-            'f9': 'Key.f9',
-            'f10': 'Key.f10',
-            'f11': 'Key.f11',
-            'f12': 'Key.f12',
-            '`': '"`"',
-            '1': '"1"',
-            '2': '"2"',
-            '3': '"3"',
-            '4': '"4"',
-            '5': '"5"',
-            '6': '"6"',
-            '7': '"7"',
-            '8': '"8"',
-            '9': '"9"',
-            '0': '"0"',
-            '-': '"-"',
-            '=': '"="',
-            'backspace': 'Key.backspace',
-            'Tab': 'Key.tab',
-            'Q': '"Q"',
-            'q': '"q"',
-            'W': '"W"',
-            'w': '"w"',
-            'E': '"E"',
-            'e': '"e"',
-            'R': '"R"',
-            'r': '"r"',
-            'T': '"T"',
-            't': '"t"',
-            'Y': '"Y"',
-            'y': '"y"',
-            'U': '"U"',
-            'u': '"u"',
-            'I': '"I"',
-            'i': '"i"',
-            'O': '"O"',
-            'o': '"o"',
-            'P': '"P"',
-            'p': '"p"',
-            '[': '"["',
-            ']': '"]"',
-            '\\': '"\\"',
-            'Caps Lock': 'Key.caps_lock',
-            'A': '"A"',
-            'a': '"a"',
-            'S': '"S"',
-            's': '"s"',
-            'D': '"D"',
-            'd': '"d"',
-            'F': '"F"',
-            'f': '"f"',
-            'G': '"G"',
-            'g': '"g"',
-            'H': '"H"',
-            'h': '"h"',
-            'J': '"J"',
-            'j': '"j"',
-            'K': '"K"',
-            'k': '"k"',
-            'L': '"L"',
-            'l': '"l"',
-            ';': '";"',
-            "'": "'",
-            'Enter': 'Key.enter',
-            'enter': 'Key.enter',
-            'L_shift': 'Key.shift',
-            'shift': 'Key.shift',
-            'Z': '"Z"',
-            'z': '"z"',
-            'X': '"X"',
-            'x': '"x"',
-            'C': '"C"',
-            'c': '"c"',
-            'V': '"V"',
-            'v': '"v"',
-            'B': '"B"',
-            'b': '"b"',
-            'N': '"N"',
-            'n': '"n"',
-            'M': '"M"',
-            'm': '"m"',
-            ',': '","',
-            '.': '"."',
-            '/': '"/"',
-            'R_shift': 'Key.shift_r',
-            'shift_r': 'Key.shift_r',
-            'L_ctrl': 'Key.ctrl_l',
-            'ctrl_l': 'Key.ctrl_l',
-            'L_win': 'Key.cmd',
-            'cmd': 'Key.cmd',
-            'L_alt': 'Key.alt_l',
-            'alt_l': 'Key.alt_l',
-            'Space': 'Key.space',
-            'space': 'Key.space',
-            'R_alt': 'Key.alt_gr',
-            'alt_gr': 'Key.alt_gr',
-            'R_win': 'Key.cmd_r',
-            'R_ctrl': 'Key.ctrl_r',
-            'ctrl_r': 'Key.ctrl_r',
+            'esc': 'Key.esc', 'f1': 'Key.f1', 'f2': 'Key.f2', 'f3': 'Key.f3', 'f4': 'Key.f4', 'f5': 'Key.f5',
+            'f6': 'Key.f6', 'f7': 'Key.f7', 'f8': 'Key.f8', 'f9': 'Key.f9', 'f10': 'Key.f10', 'f11': 'Key.f11',
+            'f12': 'Key.f12', '`': '"`"', '1': '"1"', '2': '"2"', '3': '"3"', '4': '"4"', '5': '"5"', '6': '"6"',
+            '7': '"7"', '8': '"8"', '9': '"9"', '0': '"0"', '-': '"-"', '=': '"="', 'backspace': 'Key.backspace',
+            'Tab': 'Key.tab', 'Q': '"Q"', 'q': '"q"', 'W': '"W"', 'w': '"w"', 'E': '"E"', 'e': '"e"', 'R': '"R"',
+            'r': '"r"', 'T': '"T"', 't': '"t"', 'Y': '"Y"', 'y': '"y"', 'U': '"U"', 'u': '"u"', 'I': '"I"', 'i': '"i"',
+            'O': '"O"', 'o': '"o"', 'P': '"P"', 'p': '"p"', '[': '"["', ']': '"]"', '\\': '"\\"',
+            'Caps Lock': 'Key.caps_lock', 'A': '"A"', 'a': '"a"', 'S': '"S"', 's': '"s"', 'D': '"D"', 'd': '"d"',
+            'F': '"F"', 'f': '"f"', 'G': '"G"', 'g': '"g"', 'H': '"H"', 'h': '"h"', 'J': '"J"', 'j': '"j"', 'K': '"K"',
+            'k': '"k"', 'L': '"L"', 'l': '"l"', ';': '";"', "'": "'", 'enter': 'Key.enter', 'shift': 'Key.shift',
+            'Z': '"Z"', 'z': '"z"', 'X': '"X"', 'x': '"x"', 'C': '"C"', 'c': '"c"', 'V': '"V"', 'v': '"v"', 'B': '"B"',
+            'b': '"b"', 'N': '"N"', 'n': '"n"', 'M': '"M"', 'm': '"m"', ',': '","', '.': '"."', '/': '"/"',
+            'shift_r': 'Key.shift_r', 'ctrl_l': 'Key.ctrl_l', 'cmd': 'Key.cmd', 'alt_l': 'Key.alt_l',
+            'space': 'Key.space', 'alt_gr': 'Key.alt_gr', 'cmd_r': 'Key.cmd_r', 'ctrl_r': 'Key.ctrl_r',
         }
         self.keyboard_mapping = keyboard_mapping
         self.macros_mapping = {}
@@ -314,7 +220,11 @@ class KeyboardListener:
             char = key.char
         else:
             char = key
-        self.written_script += f'self.keyboard.press({self.button_comparison.get(char)});'
+        command = f'self.keyboard.press({self.button_comparison.get(char)});'
+        release_command = f'self.keyboard.press({self.button_comparison.get(char)});'
+        if key != Key.f8 and command not in self.written_script\
+                or self.written_script.count(command) <= self.written_script.count(release_command):
+            self.written_script += f'self.keyboard.press({self.button_comparison.get(char)});'
 
     def write_release(self, key):
         if isinstance(key, Key):
@@ -323,7 +233,8 @@ class KeyboardListener:
             char = key.char
         else:
             char = key
-        self.written_script += f'self.keyboard.release({self.button_comparison.get(char)});'
+        if key != Key.f8:
+            self.written_script += f'self.keyboard.release({self.button_comparison.get(char)});'
         return key != Key.f8
 
     def write_script(self):
@@ -331,7 +242,7 @@ class KeyboardListener:
                 on_press=self.write_press,
                 on_release=self.write_release) as listener:
             listener.join()
-        return self.written_script.replace('self.keyboard.release(Key.f8);','')
+        return self.written_script.replace('self.keyboard.release(Key.f8);', '')
 
 
 if __name__ == "__main__":
